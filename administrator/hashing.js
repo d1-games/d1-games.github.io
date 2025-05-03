9cad5d8cbf8d92e8c1f9ecdca8de95fbdd5e22c1
notarcade <steve.lacrp@gmail.com>
2025-05-03 13:31:19 -0400

    Deleted all files

--- administrator/hashing.js
+++
@@ -1,9 +0,0 @@
-async function hashPassword(password) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-    
-    return Array.from(new Uint8Array(hashBuffer))
-        .map(byte => byte.toString(16).padStart(2, "0"))
-        .join("");
-}
--- administrator/session-auth.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const currentPage = window.location.pathname;
-    const isLoggedIn = localStorage.getItem("adminAuthenticated") === "true";
-
-    if (!isLoggedIn && currentPage !== "/administrator/admin-login.html") {
-        window.location.href = "/administrator/admin-login.html";
-    }
-});
